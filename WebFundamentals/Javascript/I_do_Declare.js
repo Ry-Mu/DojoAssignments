36f675635b0d4bfa8d56c94b6417556bb9ebb6fb
Ryan Munguia <ryanimunguia@gmail.com>
2017-01-30 14:28:20 -0800

    adding all of the recent files in my dojo assignments folder

---
+++ WebFundamentals/Javascript/I_do_Declare.js
@@ -0,0 +1,13 @@
+var num1 = 24.5;
+var num2 = 35;
+var num3 = 30;
+var num4 = -64;
+
+var string1 = "Hello fam";
+var string2 = "Go Warriors!";
+var string3 = "JS is cool";
+
+var iAmTired = true;
+var codingIsHard = false;
+
+// tested each case JS Bin
---
+++ WebFundamentals/Javascript/Range_print.js
@@ -0,0 +1,18 @@
+// function rangePrint(startpoint, endpoint, skipAmount){
+//   var printValue = startpoint;
+//
+//   while(printValue < endpoint){
+//     console.log(printValue);
+//     printValue += skipAmount;
+//   }
+//
+// }
+// rangePrint(2,10,2);
+
+function printRange(startpoint, endpoint, skipAmount){
+  for(var i=2; i<10; i= i+2){
+    console.log(i)
+  }
+
+}
+printRange(2,10,2);
